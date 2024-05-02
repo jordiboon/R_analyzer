ed1dc7a489bf8fb816b968c8ee8cb3017d9890af
Jordi Boon <jordi.boon01@gmail.com>
2024-05-02 16:02:02 +0200

    experimenting with visitors to extract info

---
+++ Python/scripts/test.r
@@ -0,0 +1,17 @@
+library(readr)
+library(dplyr)
+
+conf_test <- 1
+conf_test2 <- 2
+conf_test3 = "/path/to/file.csv"
+
+param_test3 = 2
+
+x <- 1
+y <- 2
+
+a = 1 + 2
+b = x + y
+
+f = list(1,2,3)
+b = read.csv("data.csv")
