fccbf44dc6feeb2c68826dbc07ca108e21f4328c
Jordi Boon <jordi.boon01@gmail.com>
2024-05-07 19:10:59 +0200

    resolve error when commment before else statement

--- Python/scripts/t2.r
+++ Python/scripts/t2.r
@@ -1,11 +1,39 @@
-Sys.setenv(
-    "AWS_ACCESS_KEY_ID" = param_s3_access_key_id,
-    "AWS_SECRET_ACCESS_KEY" = param_s3_secret_access_key,
-    "AWS_S3_ENDPOINT" = param_s3_endpoint
-    )
-datain1 = paste0(conf_output,"Phytoplankton__Progetto_Strategico_2009_2012_Australia.csv")
-datain2 = paste0(conf_output,"2_FILEinformativo_OPERATORE.csv")
-# Upload file to bucket: uploads `myfile_local.csv` to your personal folder on MinIO as `myfile.csv`
-save_object(region="", bucket="naa-vre-user-data", file=datain1, object=paste0(param_s3_prefix, "/myfile/Phytoplankton__Progetto_Strategico_2009_2012_Australia.csv"))
-save_object(region="", bucket="naa-vre-user-data", file=datain2, object=paste0(param_s3_prefix, "/myfile/2_FILEinformativo_OPERATORE.csv"))
-
+# density
+if('density'%in%CompTraits){
+  df.merged[,'density'] = rep(NA,length=nrow(df.merged))
+  # default method to calculate the density
+  if(param_CountingStrategy=='density0'){  
+    df.merged.concat = df.merged[(is.na(df.merged[,'volumeofsedimentationchamber'])) & (is.na(df.merged[,'transectcounting'])),]
+    df.temp = df.merged[!is.na(df.merged[,'volumeofsedimentationchamber']) & !is.na(df.merged[,'transectcounting']),]
+    df.temp1 = subset(df.temp,volumeofsedimentationchamber <= 5)
+    df.temp1[,'density'] = df.temp1[,'organismquantity']/df.temp1[,'transectcounting']*1000/0.001979
+    df.merged.concat = rbind(df.merged.concat,df.temp1)
+    df.temp2 = subset(df.temp,(volumeofsedimentationchamber > 5) & (volumeofsedimentationchamber <= 10))
+    df.temp2[,'density'] = df.temp2[,'organismquantity']/df.temp2[,'transectcounting']*1000/0.00365
+    df.merged.concat = rbind(df.merged.concat,df.temp2)
+    df.temp3 = subset(df.temp,(volumeofsedimentationchamber > 10) & (volumeofsedimentationchamber <= 25))
+    df.temp3[,'density'] = df.temp3[,'organismquantity']/df.temp3[,'transectcounting']*1000/0.010555
+    df.merged.concat = rbind(df.merged.concat,df.temp3)
+    df.temp4 = subset(df.temp,(volumeofsedimentationchamber > 25) & (volumeofsedimentationchamber <= 50))
+    df.temp4[,'density'] = df.temp4[,'organismquantity']/df.temp4[,'transectcounting']*1000/0.021703
+    df.merged.concat = rbind(df.merged.concat,df.temp4)
+    df.temp5 = subset(df.temp,volumeofsedimentationchamber > 50)
+    df.temp5[,'density'] = df.temp5[,'organismquantity']/df.temp5[,'transectcounting']*1000/0.041598
+    df.merged.concat = rbind(df.merged.concat,df.temp5)
+    df.merged.concat = df.merged.concat[order(df.merged.concat[,'index']),]
+    df.merged = df.merged.concat
+    df.merged[,'density'] = round(df.merged[,'density'],2)
+  }
+  else if(param_CountingStrategy=='density1'){
+    df.merged[,'areaofsedimentationchamber'] = ((df.merged[,'diameterofsedimentationchamber']/2)^2)*pi
+    df.merged[,'areaofcountingfield'] = ((df.merged[,'diameteroffieldofview']/2)^2)*pi
+    df.merged[,'density'] = round(df.merged[,'organismquantity']*1000*df.merged[,'areaofsedimentationchamber']/df.merged[,'numberofcountedfields']*df.merged[,'areaofcountingfield']*df.merged[,'settlingvolume'],2)
+  }
+  else if(param_CountingStrategy=='density2'){
+    df.merged[,'density'] = round(((df.merged[,'organismquantity']/df.merged[,'numberoftransects'])*(pi/4)*(df.merged[,'diameterofsedimentationchamber']/df.merged[,'diameteroffieldofview']))*1000/df.merged[,'settlingvolume'],2)
+  }
+  else if(param_CountingStrategy=='density3'){
+    df.merged[,'density'] = round((df.merged[,'organismquantity']*1000)/df.merged[,'settlingvolume'],2)
+  }
+  df.merged[,'density'] = df.merged[,'density']/df.merged[,'dilutionfactor']
+}   
